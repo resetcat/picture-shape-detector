ff9d2f56bf8215bd687165addc004a585aa5d133
Mihails Dudarevs <deadcatofdoom@googlemail.com>
2023-02-05 09:32:20 +0200

    refactor

--- ShapeDetector.py
+++ ShapeDetector.py
@@ -5,7 +5,7 @@ import numpy as np
 img = cv2.imread('lib/png_image.png')
 with open("lib/camera_intrinsics.json", "r") as f:
     data = json.load(f)
-fx = data["ffx"]
+h_focal_length = data["ffx"]
 real_world_distance = 380
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -14,6 +14,8 @@ contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 
 def calc_triangle():
+    x = approx.ravel()[0]
+    y = approx.ravel()[1]
     vec1 = np.array([approx[0][0][0] - approx[1][0][0], approx[0][0][1] - approx[1][0][1]])
     vec2 = np.array([approx[2][0][0] - approx[1][0][0], approx[2][0][1] - approx[1][0][1]])
     cosine_angle = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
@@ -26,7 +28,7 @@ def calc_four_angle():
     x, y, w, h = cv2.boundingRect(approx)
     aspect_ratio = float(w) / h
     side_length = max(w, h)
-    size_in_mm = (side_length * real_world_distance) / fx
+    size_in_mm = (side_length * real_world_distance) / h_focal_length
     size_in_mm = int(np.round(size_in_mm))
     if 0.95 <= aspect_ratio < 1.05:
         cv2.putText(img, "square side length is " + str(size_in_mm) + "mm", (x, y),
@@ -38,7 +40,7 @@ def calc_four_angle():
 
 def calc_circle():
     (x, y), radius = cv2.minEnclosingCircle(approx)
-    radius_mm = (radius * real_world_distance) / fx
+    radius_mm = (radius * real_world_distance) / h_focal_length
     radius_mm = int(np.round(radius_mm))
     cv2.putText(img, "circle radius is " + str(radius_mm) + "mm",
                 (int(x), int(y + radius_mm * 3)), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0), 1)
@@ -47,8 +49,6 @@ def calc_circle():
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
     cv2.drawContours(img, [approx], 0, (0, 255, 255), 2)
-    x = approx.ravel()[0]
-    y = approx.ravel()[1] - 5
     if len(approx) == 3:
         calc_triangle()
     elif len(approx) == 4:
